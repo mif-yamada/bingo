48c9f1f13a9b6e305505930f71fda2291f8457a2
mosa <mosa@mosanoMacBook-Pro.local>
2021-11-09 18:01:33 +0900

    useState利用、ball取り出し途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Button } from './component/button';
 import { Card } from './component/card';
-import { createRandomCardNumList } from "./utils/createCard";
-import { createRandomNumberList } from './utils/random';
+import { createRandomCardNumList } from './utils/createCard';
+import { noDuplicationRandomNum } from './utils/random';
 
-function App() {
+const App: React.FC = () => {
+  const [ball, setBall] = useState<number>(0);
+  const [ballNumList, setBallNumList] = useState<number[]>([0]);
+
+  const getBall = () => {
+    if (ballNumList.length < 76) {
+      const RandomNum = noDuplicationRandomNum(1, 75, ballNumList);
+    }
+  };
+
+
   return (
     <div className="App">
       <Button title='bingo' onClick={() => { }}></Button>
--- src/component/card/index.tsx
+++ src/component/card/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Cell } from '../cell';
-// import { createNoDuplicationSortRandomNumList} from "../../utils/random";
 
 interface CardProps{
   cardNumberList: number[][];
-  patchList?: number[];
+  ballNumList?: number[];
 }
 
 const StyledCard = styled.table`
@@ -20,15 +19,13 @@ const StyledCard = styled.table`
 
 const Card: React.FC<CardProps> = (props: CardProps) => {
   const {
-    cardNumberList,patchList
+    cardNumberList,ballNumList
   } = props
-  const isBlanked = (val: number) => patchList?.includes(val);
-  // const colNumbers = createNoDuplicationSortRandomNumList();
+  const isBlanked = (val: number) => val===0||ballNumList?.includes(val);
   return (
     <StyledCard>
       <tbody>
         {cardNumberList.map((li, i) => {
-          //Htmlの表示を変えるか、入れるデータで向きを変えるか
         return (<tr key={li[i]}>{
             li.map((val: number, idx: number) => {
               return (
