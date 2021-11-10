43a6d61b4dc236319d3f6e4310531e6c9dadca3a
mosa <mosa@mosanoMacBook-Pro.local>
2021-11-10 12:21:48 +0900

    bingo判定　途中

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,46 @@ const App: React.FC = () => {
   const [ballNumList, setBallNumList] = useState<number[]>([0]);
   const [lostAllBall, setLostAllBall] = useState<boolean>(false);
   const [cardList, setCardList] = useState<number[][]>([]);
-  const [strCardList, setSterCardList] = useState([]);
+  const [colCheckCardList, setColCheckCardList] = useState<number[][]>([]);
 
-  // useEffect = () => {
-  //   const initCard = createRandomCardNumList();
+  useEffect(() => {
+    const initCard = createRandomCardNumList();
+    setCardList(initCard.cardNumList);
+    setColCheckCardList(initCard.centerFreeNumList);
+  }, []);
 
-  // }
   const getBall = () => {
     //カードチェックし、穴を開ける
     if (ballNumList.length < 76) {
       const ballNum = noDuplicationRandomNum(1, 75, ballNumList);
       setBall(ballNum);
       setBallNumList(ballNumList.concat(ballNum));
-      console.log(`${ball},${ballNumList}`)
     } else {
       setLostAllBall(true);
     }
   };
 
+  //斜めリスト
+  const crossCheckCardList = [
+    [cardList[0][0], cardList[1][1], 0, cardList[3][3], cardList[4][4]],
+    [cardList[0][4], cardList[1][3], 0, cardList[3][1], cardList[4][0]],
+  ];
+  //TODO: bingoreachチェックに使用
+  const checkCardNum = (checkList: number[][], ballnum: number) => {
+    //.lengthの値で判定
+    const checkCount = checkList.map((array, idx, list) => {
+      const checkarray: number[] = array.filter((val) => val === ballnum);
+      return checkarray.length;
+    });
+  };
 
+  //TODO:ボールを引いたら動くもの
+  useEffect(() => {}, [ballNumList]);
 
   return (
     <div className="App">
-      <Button title='bingo' onClick={() =>getBall()}></Button>
-      <Card cardNumberList={strCardList}></Card>
+      <Button title="bingo" onClick={() => getBall()}></Button>
+      <Card cardNumberList={cardList} ballNumList={ballNumList}></Card>
     </div>
   );
 }
--- src/utils/createCard.ts
+++ src/utils/createCard.ts
@@ -2,21 +2,27 @@ import { createNoDuplicationSortRandomNumList } from './random';
 
 export const createRandomCardNumList = () => {
   const size = 5;
-  const randomNumList:number[][] = [...Array(5)].map((val, idx, colList) => {
+  const randomNumList: number[][] = [...Array(5)].map((val, idx, colList) => {
     colList = createNoDuplicationSortRandomNumList(idx * 15 + 1, (idx + 1) * 15, 5);
     return colList;
   });
-
-  //行列入れ替え、中心FREE
-  const cardNumList= randomNumList.map((colVals, colIdx, numList) => {
-    const rowVals = colVals.map((val, idx)=>{
+  //中心FREE=0　縦の判定に使う
+  const centerFreeNumList = randomNumList.map((colVals, colIdx) => {
+    const centerFreeList = colVals.map((val, idx) => {
       if (colIdx === 2 && idx === 2) {
         return val = 0;
       } else {
-        return val = numList[idx][colIdx];
+        return val;
       }
     });
+    return centerFreeList;
+  });
+  //行列入れ替え 横の判定に使う
+  const cardNumList = centerFreeNumList.map((colVals, colIdx, numList) => {
+    const rowVals = colVals.map((val, idx) => {
+      return (val = numList[idx][colIdx]);
+    });
     return rowVals;
-  })
-  return cardNumList;
+  });
+  return {cardNumList,centerFreeNumList};
 };
