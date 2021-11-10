25b7101068c35fa99cc063277a834ebb0dd1f45a
mosa <mosa@mosanoMacBook-Pro.local>
2021-11-10 10:15:40 +0900

    11/10 朝変更

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,32 @@ import { noDuplicationRandomNum } from './utils/random';
 const App: React.FC = () => {
   const [ball, setBall] = useState<number>(0);
   const [ballNumList, setBallNumList] = useState<number[]>([0]);
+  const [lostAllBall, setLostAllBall] = useState<boolean>(false);
+  const [cardList, setCardList] = useState<number[][]>([]);
+  const [strCardList, setSterCardList] = useState([]);
 
+  // useEffect = () => {
+  //   const initCard = createRandomCardNumList();
+
+  // }
   const getBall = () => {
-    //ダブりがなきゃボールリストに入れる
     //カードチェックし、穴を開ける
     if (ballNumList.length < 76) {
-      const RandomNum = noDuplicationRandomNum(1, 75, ballNumList);
-      setBallNumList(ballNumList.filter((val) => { ballNumList.includes(RandomNum)}))
-      }
+      const ballNum = noDuplicationRandomNum(1, 75, ballNumList);
+      setBall(ballNum);
+      setBallNumList(ballNumList.concat(ballNum));
+      console.log(`${ball},${ballNumList}`)
+    } else {
+      setLostAllBall(true);
+    }
   };
+
+
+
   return (
     <div className="App">
-      <Button title='bingo' onClick={() => { }}></Button>
-      <Card cardNumberList={createRandomCardNumList()}></Card>
+      <Button title='bingo' onClick={() =>getBall()}></Button>
+      <Card cardNumberList={strCardList}></Card>
     </div>
   );
 }
