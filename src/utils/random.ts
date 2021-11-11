1b3ea8521d100b97f78b0c4e2443c07fdc91e057
mosa <mosa@mosanoMacBook-Pro.local>
2021-11-11 15:01:34 +0900

    ESLint使用コード

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,28 +1,43 @@
 export const randomNumber = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-export const noDuplicationRandomNum = (min: number, max: number, ballList: number[]):number => {
+export const noDuplicationRandomNum = (
+  min: number,
+  max: number,
+  ballList: number[]
+): number => {
   const getNum: number = randomNumber(min, max);
-  if (ballList.includes(getNum)){
+  if (ballList.includes(getNum)) {
     return noDuplicationRandomNum(min, max, ballList);
   }
   return getNum;
-}
+};
 
-export const createRandomNumList = (min: number, max: number, length: number) => {
+export const createRandomNumList = (
+  min: number,
+  max: number,
+  length: number
+) => {
   const randomNumList = [...Array(length)].map(() => randomNumber(min, max));
   return Array.from(randomNumList);
-}
+};
 
-export const createNoDuplicationRandomNumberList = (min: number, max: number, size:number) => {
+export const createNoDuplicationRandomNumberList = (
+  min: number,
+  max: number,
+  size: number
+) => {
   const joinRandomNumList = (array: number[], joinElementNum: number) => {
-    const joinList = createRandomNumList(min,max,joinElementNum);
+    const joinList = createRandomNumList(min, max, joinElementNum);
     return Array.from(new Set([...array, ...joinList]));
   };
-  const initNumList = Array.from(new Set(createRandomNumList(min,max,size)));
+  const initNumList = Array.from(new Set(createRandomNumList(min, max, size)));
   const createNoDuplicationRandomNumList = (): number[] => {
-    const noDuplicationRandomNumList = joinRandomNumList(initNumList,size - initNumList.length);
+    const noDuplicationRandomNumList = joinRandomNumList(
+      initNumList,
+      size - initNumList.length
+    );
     if (size !== noDuplicationRandomNumList.length) {
       return createNoDuplicationRandomNumList();
     }
@@ -30,4 +45,4 @@ export const createNoDuplicationRandomNumberList = (min: number, max: number, si
   };
   const list = createNoDuplicationRandomNumList();
   return list;
-}
+};
